98fcfcb42d62b8694989b7b89ad6117f13085f42
Villal Fedora <valentinvillalba.xyz@gmail.com>
2023-01-17 10:44:26 -0300

    nuevo cartel y fixes en formulario y responsive

--- index.js
+++ index.js
@@ -24,9 +24,35 @@ function cambiarImagen(){
     }
 }
 
+function checkform(form) {
+    let pattern = "/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/";
+
+    // get all the inputs within the submitted form
+    let inputs = form.getElementsByTagName('input');
+    for (let i = 0; i < inputs.length; i++) {
+        // only validate the inputs that have the required attribute
+        if(inputs[i].hasAttribute("required")){
+            if(inputs[i].value == ""){
+                // found an empty field that is required
+                document.getElementById("advertencia-form").style.display = "block";
+                return false;
+            }
+        }
+    }
+
+    if((document.getElementById("campo-email").value).match(pattern) == null){
+        document.getElementById("advertencia-form").style.display = "block";
+        return false;
+    }
+
+    enviarMensaje();
+    return true;
+}
+
 function enviarMensaje(){
     document.getElementById("caja-mensaje").style.display = "none";
     document.getElementById("cosas-de-arriba").style.display = "none";
+    document.getElementById("caja-tcg").style.display = "none";
     document.getElementById("enviando-mensaje").style.display = "block";
 }
 
